be95d47aed3006d4fd5506eb907d2c1746a4e428
hugo ramirez <hugo.ramirez@academy.local>
2026-04-07 16:07:43 -0300

    comentado

--- micliente.py
+++ micliente.py
@@ -1,13 +1,13 @@
-import socket
-import threading
+import socket#libreria para crear los sockets
+import threading#libreria para crear los hilos
 import time
 
 def conectar():
     while True:
         try:
-            nuevo_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            nuevo_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#creamos un socket con la familia de direcciones ipv4 y el protocolo tcp
             # Intentamos conectar
-            nuevo_socket.connect(('127.0.0.1', 55555))
+            nuevo_socket.connect(('127.0.0.1', 55555))#conectamos al servidor con la ip local y el puerto del servidor 
             print("[SISTEMA] ¡Conexión establecida!")
             return nuevo_socket
         except:
@@ -17,33 +17,33 @@ def conectar():
 def buzon(cliente_actual):
     while True:
         try:
-            mensaje = cliente_actual.recv(1024).decode('utf-8')
+            mensaje = cliente_actual.recv(1024).decode('utf-8')#decode transforma el recv en str y utf-8 el estandar de codificacion de caracteres, el 1024 es el tamaño del buffer
             if not mensaje:
-                raise Exception("Servidor cerrado")
+                raise Exception("Servidor cerrado")#para que no este esperando infinitamente
             print(f"\n{mensaje}")
-        except:
+        except:#si el servidor se cae o se cierra, lo detectamos y reiniciamos el cliente
             print("\n[!] El servidor se ha desconectado.")
             cliente_actual.close()
             reiniciar_cliente()
             break
 
-def enviar(cliente_actual):
+def enviar(cliente_actual):#funcion para enviar mensajes al servidor, se ejecuta en el hilo principal
     while True:
         try:
-            texto = input("CHAT:")
+            texto = input("CHAT:")#input es bloqueante, espera a que el usuario escriba algo y presione enter
             cliente_actual.send(f"{NOMBRE}: {texto}".encode('utf-8'))
         except:
           
             break
 
-def reiniciar_cliente():
+def reiniciar_cliente():#si el servidor se cae, reiniciamos el cliente para que intente reconectar
     global CLIENTE
     CLIENTE = conectar()
 
 
 NOMBRE = input("TU NOMBRE: ")
 CLIENTE = conectar()
+#el hilo de cliente si tiene daemnon para que no se queden los procesos colgando y tener un cierre limpio
+threading.Thread(target=buzon, args=(CLIENTE,), daemon=True).start()#iniciamos un hilo para escuchar los mensajes del servidor, el daemon=True hace que el hilo se cierre cuando el programa principal termine
 
-threading.Thread(target=buzon, args=(CLIENTE,), daemon=True).start()
-
-enviar(CLIENTE)
+enviar(CLIENTE)#iniciamos la funcion de enviar mensajes al servidor en el hilo principal, esto es porque el input es bloqueante y no queremos que el hilo de buzon se quede esperando a que el usuario escriba algo para mostrar los mensajes del servidor
--- miservidor.py
+++ miservidor.py
@@ -1,53 +1,54 @@
-import socket
-import threading
+import socket#libreria para crear los sockets
+import threading#libreria para crear los hilos
 
-HOST = "0.0.0.0"
-PORT = 55555
+HOST = "0.0.0.0"#nuestra direccion IP en la red, 0.0.0.0 escucha en todas las interfaces
+PORT = 55555#nuestro puerto para acceder al servicio
 
-SERVIDOR = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SERVIDOR.bind((HOST, PORT))
-SERVIDOR.listen()
+SERVIDOR = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#af_inet familia de direcciones tipo ipv4 y protoclo tcp 
+SERVIDOR.bind((HOST, PORT))#vinculamos con el socket la ip y el puerto
+SERVIDOR.listen()#haces que escuch o mejor dicho que espere conexiones
 
-CONECTADOS = []
+CONECTADOS = []#aqui se encontraran los clientes conectados
 
 def chat_global(mensaje, emisor):
     # Usamos una copia de la lista para evitar errores si alguien se desconecta justo ahora
-    for usuario in list(CONECTADOS):
+    for usuario in list(CONECTADOS):#creamo una copia superficial para un beun manejo de rrores
         try:
             # Enviamos a todos excepto a quien lo envió (opcional)
             if usuario != emisor:
-                usuario.send(mensaje)
+                usuario.send(mensaje) 
         except:
             # Si falla el envío, detectamos que el socket está MUERTO
             eliminar_usuario(usuario)
 
-def eliminar_usuario(usuario):
+def eliminar_usuario(usuario):#cuando un cliente se desconecta, lo eliminamos de la lista de conectados y cerramos su socket
     if usuario in CONECTADOS:
         CONECTADOS.remove(usuario)
         usuario.close()
         print("¡Un cliente se fue! Lista actualizada.")
 
-def atencion_cliente(usuario):
+def atencion_cliente(usuario):#funcion que se ejecuta en un hilo para cada cliente conectado, se encarga de recibir mensajes y enviarlos a los demas clientes
     while True:
         try:
-            mensaje = usuario.recv(1024)
+            mensaje = usuario.recv(1024)#blocking, espera a recibir un mensaje del cliente, el 1024 es el tamaño del buffer
             if not mensaje: # Si recibimos un mensaje vacío, el cliente se desconectó
-                raise Exception("Cliente desconectado")
+                raise Exception("Cliente desconectado")#lanzamos una excepción para salir del bucle y manejar la desconexión
             chat_global(mensaje, usuario)
         except:
             # Manejamos la desconexión con elegancia
             eliminar_usuario(usuario)
             break
 
+#obs= el servidor no tiene daemon para siempre este activo, esto maneja la multiples conexiones de clientes
 def iniciar():
     print("SERVIDOR ESTABLE CORRIENDO...")
     while True:
         try:
-            usuario, ubi = SERVIDOR.accept()
+            usuario, ubi = SERVIDOR.accept()#el socket del cliente y puerto e IP
             CONECTADOS.append(usuario)
-            thread = threading.Thread(target=atencion_cliente, args=(usuario,))
-            thread.start()
+            thread = threading.Thread(target=atencion_cliente, args=(usuario,))#le damos al hilo una funcion y su argumento
+            thread.start()#iniciamos el hilo para atender al cliente
         except Exception as e:
             print(f"Error aceptando conexión: {e}")
 
-iniciar()
+iniciar()#iniciamos el servidor y comenzamos a aceptar conexiones de clientes
